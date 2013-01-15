83df2a2001b4fa4b77af6cea00fbde1bd9061790
thekraven <savethestyle1@gmail.com>
2013-01-14 18:00:52 -0600

    fix cdma baseband
    Change-Id: I125a7d5e051481235bb11b565b99a640b9880c57

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -33,8 +33,11 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
